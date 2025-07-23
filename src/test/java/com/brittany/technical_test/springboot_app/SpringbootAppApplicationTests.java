c7ea919b041c136721aa95780d94bacf4edc47e0
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-22 20:15:58 -0500

    feat: Agregar archivos de configuración y estructura inicial del proyecto Spring Boot

---
+++ src/test/java/com/brittany/technical_test/springboot_app/SpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brittany.technical_test.springboot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
