cb2809bd79ac8e6b7ad2b4e5a1041f15b95bc96a
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-23 16:06:04 -0500

    fix: corregir tipo de dato de 'id' en ClienteResponseDTO a Long

--- src/main/java/com/brittany/technical_test/springboot_app/DTOs/Response/ClienteResponseDTO.java
+++ src/main/java/com/brittany/technical_test/springboot_app/DTOs/Response/ClienteResponseDTO.java
@@ -3,7 +3,7 @@ package com.brittany.technical_test.springboot_app.DTOs.Response;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonPropertyOrder({ "message", "id", "numCedula", "nombres", "direccion", "telefono", "genero" })
-public record ClienteResponseDTO(String message, String id, String numCedula, String nombres, String direccion,
+public record ClienteResponseDTO(String message, Long id, String numCedula, String nombres, String direccion,
         String telefono, String genero) {
 
 }
