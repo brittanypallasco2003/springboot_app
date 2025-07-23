b3ddf6e67b59570b4620488395ed2650a2b6f625
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-23 16:17:35 -0500

    fix: corregir tipo de entidad en CuentaRepository para usar Cuenta en lugar de CuentaRepository

--- src/main/java/com/brittany/technical_test/springboot_app/repositories/CuentaRepository.java
+++ src/main/java/com/brittany/technical_test/springboot_app/repositories/CuentaRepository.java
@@ -2,6 +2,8 @@ package com.brittany.technical_test.springboot_app.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CuentaRepository extends JpaRepository<CuentaRepository, String> {
+import com.brittany.technical_test.springboot_app.models.Cuenta;
+
+public interface CuentaRepository extends JpaRepository<Cuenta, String> {
 
 }
