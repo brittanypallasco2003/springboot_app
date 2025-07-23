cb87b1aee31da002984d818a4de2ce4380f72151
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-23 16:06:34 -0500

    feat: implementar método createCliente en ClienteServiceImpl para crear un nuevo cliente

--- src/main/java/com/brittany/technical_test/springboot_app/services/ClienteService.java
+++ src/main/java/com/brittany/technical_test/springboot_app/services/ClienteService.java
@@ -6,7 +6,8 @@ import com.brittany.technical_test.springboot_app.DTOs.Request.ClienteCreateDTO;
 import com.brittany.technical_test.springboot_app.DTOs.Response.ClienteResponseDTO;
 
 public interface ClienteService {
-
+    
+    ClienteResponseDTO createCliente(ClienteCreateDTO dto);
     boolean existsByPhoneNumber(String phoneNumber);
     boolean existsByDni(String dni);
 }
--- src/main/java/com/brittany/technical_test/springboot_app/services/ClienteServiceImpl.java
+++ src/main/java/com/brittany/technical_test/springboot_app/services/ClienteServiceImpl.java
@@ -1,8 +1,12 @@
 package com.brittany.technical_test.springboot_app.services;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.brittany.technical_test.springboot_app.DTOs.Request.ClienteCreateDTO;
+import com.brittany.technical_test.springboot_app.DTOs.Response.ClienteResponseDTO;
+import com.brittany.technical_test.springboot_app.models.Cliente;
 import com.brittany.technical_test.springboot_app.repositories.ClienteRepository;
 
 @Service
@@ -25,6 +29,37 @@ public class ClienteServiceImpl implements ClienteService {
     @Override
     public boolean existsByDni(String dni) {
         return clienteRepository.existsByNumCedula(dni);
+    }
+
+    @Transactional
+    @Override
+    public ClienteResponseDTO createCliente(ClienteCreateDTO dto) {
+
+        String passwordEncriptada=BCrypt.hashpw(dto.password(), BCrypt.gensalt());
+        Cliente cliente= Cliente.builder()
+        .numCedula(dto.numCedula())
+        .nombre(dto.nombres())
+        .password(passwordEncriptada)
+        .direccion(dto.direccion())
+        .telefono(dto.telefono())
+        .genero(dto.genero())
+        .edad(dto.edad())
+        .estado(true)
+        .build(); 
+
+        Cliente clienteCreated=clienteRepository.save(cliente);
+        return mapClienteResposeDTO(clienteCreated, "Usuario creado exitosamente");
+    }
+
+
+    private ClienteResponseDTO mapClienteResposeDTO(Cliente cliente, String message){
+        return new ClienteResponseDTO(message, 
+            cliente.getClientId(), 
+            cliente.getNumCedula(), 
+            cliente.getNombre(), 
+            cliente.getDireccion(), 
+            cliente.getTelefono(), 
+            cliente.getGenero());
     }
 
 }
