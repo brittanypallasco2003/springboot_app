faa7b6edb569bb3c790f53d8034a4df9996338d0
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-23 22:57:12 -0500

    refactor: agregar anotaciones @Target y @Retention a la anotación NotBlankIfPresent

--- src/main/java/com/brittany/technical_test/springboot_app/validators/annotations/NotBlankIfPresent.java
+++ src/main/java/com/brittany/technical_test/springboot_app/validators/annotations/NotBlankIfPresent.java
@@ -1,11 +1,24 @@
 package com.brittany.technical_test.springboot_app.validators.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import com.brittany.technical_test.springboot_app.validators.NotBlankIfPresentValidator;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
+/**
+ * Valida que si el campo está presente (no es null), no esté en blanco.
+ * Permite valores nulos (por ejemplo, en actualizaciones parciales tipo PATCH),
+ * pero no permite cadenas vacías o solo espacios si el campo es enviado.
+ */
+
 @Constraint(validatedBy = NotBlankIfPresentValidator.class)
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface NotBlankIfPresent {
     String message() default "El campo no puede estar en blanco si está presente";
 
