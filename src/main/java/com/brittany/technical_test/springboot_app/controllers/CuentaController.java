33bc1691e6f2418831562c337f20063043694cb8
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-24 13:12:33 -0500

    feat: agregar endpoint POST en CuentaController para creación de cuentas

---
+++ src/main/java/com/brittany/technical_test/springboot_app/controllers/CuentaController.java
@@ -0,0 +1,44 @@
+package com.brittany.technical_test.springboot_app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.brittany.technical_test.springboot_app.DTOs.Request.CuentaCreateDTO;
+import com.brittany.technical_test.springboot_app.DTOs.Response.CuentaResponseDTO;
+import com.brittany.technical_test.springboot_app.services.CuentaService;
+
+import jakarta.validation.Valid;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/cuentas")
+public class CuentaController {
+
+    private final CuentaService cuentaService;
+
+    public CuentaController(CuentaService cuentaService) {
+        this.cuentaService = cuentaService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> postCuenta(@RequestParam Long clientId, @Valid @RequestBody CuentaCreateDTO dto) {
+
+        CuentaResponseDTO response = cuentaService.crearCuenta(clientId, dto);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path("cuentas/{id}")
+                .buildAndExpand(response.id())
+                .toUri();
+
+        return ResponseEntity.created(location).body(response);
+    }
+
+}
