769cacda32c768ad7a259a5aee654f9120ff28e4
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-24 13:38:03 -0500

    feat: agregar endpoint GET en CuentaController para obtener una cuenta por su id

--- src/main/java/com/brittany/technical_test/springboot_app/controllers/CuentaController.java
+++ src/main/java/com/brittany/technical_test/springboot_app/controllers/CuentaController.java
@@ -12,10 +12,13 @@ import com.brittany.technical_test.springboot_app.services.CuentaService;
 import jakarta.validation.Valid;
 
 import java.net.URI;
+import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/cuentas")
@@ -39,6 +42,11 @@ public class CuentaController {
                 .toUri();
 
         return ResponseEntity.created(location).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getAllCuentas(@PathVariable UUID id) {
+        return ResponseEntity.ok().body(cuentaService.getSpecificCuenta(id));
     }
 
 }
