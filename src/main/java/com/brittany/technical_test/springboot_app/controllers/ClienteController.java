7f0cc78c27d317c0177bd09f15a27181bddae0a3
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-24 00:07:30 -0500

    refactor: agregar endpoint PATCH en ClienteController para actualización parcial de los datos

--- src/main/java/com/brittany/technical_test/springboot_app/controllers/ClienteController.java
+++ src/main/java/com/brittany/technical_test/springboot_app/controllers/ClienteController.java
@@ -18,13 +18,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
-
 
     private final ClienteService clienteService;
 
@@ -55,10 +55,17 @@ public class ClienteController {
         return ResponseEntity.ok().body(clienteService.getSpecificClient(id));
     }
 
-    @PutMapping("update/{id}")
-    public ResponseEntity<?> putMethodName(@PathVariable Long id, @Valid @RequestBody ClienteUpdateDTO dto) {
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> replaceClient(@PathVariable Long id, @Valid @RequestBody ClienteUpdateDTO dto) {
 
         return ResponseEntity.ok().body(clienteService.updateClient(dto, id));
+    }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<?> patchCLient(@PathVariable Long id,
+            @Valid @RequestBody ClienteUpdateDTO dto) {
+        return ResponseEntity.ok().body(clienteService.parcialUpdateCliente(dto, id));
+
     }
 
 }
