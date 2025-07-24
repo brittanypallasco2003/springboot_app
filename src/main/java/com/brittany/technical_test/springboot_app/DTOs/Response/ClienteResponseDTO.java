b597f20f0d1bb09ff231ece965c31ba0fcaafdea
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-24 11:59:45 -0500

    refactor: mover la anotación @JsonInclude a nivel de clase para evitar la serialización de campos no enviados en la respuesta

--- src/main/java/com/brittany/technical_test/springboot_app/DTOs/Response/ClienteResponseDTO.java
+++ src/main/java/com/brittany/technical_test/springboot_app/DTOs/Response/ClienteResponseDTO.java
@@ -1,12 +1,12 @@
 package com.brittany.technical_test.springboot_app.DTOs.Response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({ "message", "id", "numCedula", "nombres", "direccion", "telefono", "genero", "estado" })
 public record ClienteResponseDTO(
-                @JsonInclude(JsonInclude.Include.NON_NULL) @JsonProperty("message") String message,
+                String message,
                 Long id,
                 String numCedula,
                 String nombres,
