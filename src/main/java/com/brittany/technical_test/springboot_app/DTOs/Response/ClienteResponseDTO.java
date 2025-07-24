c722d2336b83641ac28c6689b18e167238293a03
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-23 21:29:59 -0500

    feat: agregar campo estado en ClienteResponseDTO y agregar @JsonInclude.NON_NULL en el campo message

--- src/main/java/com/brittany/technical_test/springboot_app/DTOs/Response/ClienteResponseDTO.java
+++ src/main/java/com/brittany/technical_test/springboot_app/DTOs/Response/ClienteResponseDTO.java
@@ -1,9 +1,19 @@
 package com.brittany.technical_test.springboot_app.DTOs.Response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({ "message", "id", "numCedula", "nombres", "direccion", "telefono", "genero" })
-public record ClienteResponseDTO(String message, Long id, String numCedula, String nombres, String direccion,
-        String telefono, String genero) {
+@JsonPropertyOrder({ "message", "id", "numCedula", "nombres", "direccion", "telefono", "genero", "estado" })
+public record ClienteResponseDTO(
+                @JsonInclude(JsonInclude.Include.NON_NULL) @JsonProperty("message") String message,
+                Long id,
+                String numCedula,
+                String nombres,
+                String direccion,
+                String telefono,
+                String genero,
+                Boolean estado
+                ) {
 
 }
