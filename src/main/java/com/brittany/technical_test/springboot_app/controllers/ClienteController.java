a3470a97e0f2d361e93c6bf5b658cafe2e620db3
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-24 00:20:41 -0500

    feat: agregar método deleteClient en ClienteController para eliminar un cliente

--- src/main/java/com/brittany/technical_test/springboot_app/controllers/ClienteController.java
+++ src/main/java/com/brittany/technical_test/springboot_app/controllers/ClienteController.java
@@ -17,6 +17,7 @@ import java.net.URI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -66,6 +67,12 @@ public class ClienteController {
             @Valid @RequestBody ClienteUpdateDTO dto) {
         return ResponseEntity.ok().body(clienteService.parcialUpdateCliente(dto, id));
 
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteClient(@PathVariable Long id) {
+        clienteService.deleteClient(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
