8fd6157c15050b2c0af4915920d450e915ca6d73
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-23 23:51:33 -0500

    refactor: actualizar ClienteController para incluir métodos de obtención y actualización de clientes

--- src/main/java/com/brittany/technical_test/springboot_app/controllers/ClienteController.java
+++ src/main/java/com/brittany/technical_test/springboot_app/controllers/ClienteController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.brittany.technical_test.springboot_app.DTOs.Request.ClienteCreateDTO;
+import com.brittany.technical_test.springboot_app.DTOs.Request.ClienteUpdateDTO;
 import com.brittany.technical_test.springboot_app.DTOs.Response.ClienteResponseDTO;
+import com.brittany.technical_test.springboot_app.repositories.ClienteRepository;
 import com.brittany.technical_test.springboot_app.services.ClienteService;
 
 import jakarta.validation.Valid;
@@ -15,30 +17,48 @@ import java.net.URI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
 
+
     private final ClienteService clienteService;
 
-    public ClienteController(ClienteService clienteService) {
+    public ClienteController(ClienteService clienteService, ClienteRepository clienteRepository) {
         this.clienteService = clienteService;
     }
 
     @PostMapping("/create")
-    public ResponseEntity<ClienteResponseDTO> postCliente(@RequestBody @Valid ClienteCreateDTO dto) {
-        ClienteResponseDTO response= clienteService.createCliente(dto);
+    public ResponseEntity<?> postCliente(@RequestBody @Valid ClienteCreateDTO dto) {
+        ClienteResponseDTO response = clienteService.createCliente(dto);
 
-        URI location= ServletUriComponentsBuilder
-        .fromCurrentRequest()
-            .path("/{id}")
-            .buildAndExpand(response.id())
-            .toUri();
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.id())
+                .toUri();
 
         return ResponseEntity.created(location).body(response);
     }
-    
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllClients() {
+        return ResponseEntity.ok().body(clienteService.listAllClients());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getSpecificClient(@PathVariable Long id) {
+        return ResponseEntity.ok().body(clienteService.getSpecificClient(id));
+    }
+
+    @PutMapping("update/{id}")
+    public ResponseEntity<?> putMethodName(@PathVariable Long id, @Valid @RequestBody ClienteUpdateDTO dto) {
+
+        return ResponseEntity.ok().body(clienteService.updateClient(dto, id));
+    }
 
 }
