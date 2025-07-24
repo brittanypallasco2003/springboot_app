28338f2bab31cb8834df645023dd301cd19c4bf3
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-24 16:22:38 -0500

    feat: agregar endpoint DELETE en CuentaController para elimnar cuentas por su id.

--- src/main/java/com/brittany/technical_test/springboot_app/controllers/CuentaController.java
+++ src/main/java/com/brittany/technical_test/springboot_app/controllers/CuentaController.java
@@ -17,6 +17,7 @@ import java.util.UUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -49,15 +50,21 @@ public class CuentaController {
     public ResponseEntity<?> getAllCuentas() {
         return ResponseEntity.ok().body(cuentaService.listAllCuentas());
     }
-    
+
     @GetMapping("/{id}")
     public ResponseEntity<?> getAllCuentas(@PathVariable UUID id) {
         return ResponseEntity.ok().body(cuentaService.getSpecificCuenta(id));
     }
 
     @PatchMapping("/update-state/{id}")
-    public ResponseEntity<?> updateCuentaState(@PathVariable UUID id){
+    public ResponseEntity<?> updateCuentaState(@PathVariable UUID id) {
         return ResponseEntity.ok().body(cuentaService.updateCuentaState(id));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteCuenta(@PathVariable UUID id) {
+        cuentaService.deleteCuenta(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
