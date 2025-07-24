a0eac634379a9399d6458be885cf0a45f7137393
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-24 12:04:57 -0500

    feat: añadir método para verificar si un cliente tiene asociado un tipo de cuenta, evitando duplicados

--- src/main/java/com/brittany/technical_test/springboot_app/repositories/CuentaRepository.java
+++ src/main/java/com/brittany/technical_test/springboot_app/repositories/CuentaRepository.java
@@ -1,9 +1,14 @@
 package com.brittany.technical_test.springboot_app.repositories;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.brittany.technical_test.springboot_app.models.Cuenta;
+import com.brittany.technical_test.springboot_app.models.TipoCuentaEnum;
 
-public interface CuentaRepository extends JpaRepository<Cuenta, String> {
+public interface CuentaRepository extends JpaRepository<Cuenta, UUID> {
+
+    boolean existsByClienteIdAndTipoCuenta(Long clienteId, TipoCuentaEnum tipoCuentaEnum);
 
 }
