984e63ade00d0646f0e4ffd999fc7ff844a1e70e
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-24 20:29:25 -0500

    refactor: usar String en lugar de UUID como id en el dto de respuesta de creación, consultas y actualizaciones de cuentas.

--- src/main/java/com/brittany/technical_test/springboot_app/DTOs/Response/CuentaResponseDTO.java
+++ src/main/java/com/brittany/technical_test/springboot_app/DTOs/Response/CuentaResponseDTO.java
@@ -1,13 +1,12 @@
 package com.brittany.technical_test.springboot_app.DTOs.Response;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
 import com.brittany.technical_test.springboot_app.models.TipoCuentaEnum;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonPropertyOrder({ "id", "numCuenta", "tipoCuenta", "saldoInicial", "saldoDisponible", "estado", "clienteResponseDTO" })
-public record CuentaResponseDTO(UUID id, String numCuenta, TipoCuentaEnum tipoCuenta, BigDecimal saldoInicial,
+public record CuentaResponseDTO(String id, String numCuenta, TipoCuentaEnum tipoCuenta, BigDecimal saldoInicial,
                 BigDecimal saldoDisponible,
                 Boolean estado, ClienteResponseDTO cliente) {
 
