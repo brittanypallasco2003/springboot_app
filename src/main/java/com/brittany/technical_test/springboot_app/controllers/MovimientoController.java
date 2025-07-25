fcc2708608e770206fbae0fe02948db44fc7cef8
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-25 00:53:18 -0500

    feat: agregar endpoint POST y GET en MovimientoController registrar un nuevo movimiento y listar todos.

---
+++ src/main/java/com/brittany/technical_test/springboot_app/controllers/MovimientoController.java
@@ -0,0 +1,52 @@
+package com.brittany.technical_test.springboot_app.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.brittany.technical_test.springboot_app.DTOs.Request.RegistroMovimientoDTO;
+import com.brittany.technical_test.springboot_app.DTOs.Response.RegistroMovimientoResponseDTO;
+import com.brittany.technical_test.springboot_app.services.MovimientoService;
+
+import jakarta.validation.Valid;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+@RequestMapping("/movimientos")
+public class MovimientoController {
+
+    private final MovimientoService movimientoService;
+
+    public MovimientoController(MovimientoService movimientoService) {
+        this.movimientoService = movimientoService;
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<?> postMovimiento(@Valid @RequestBody RegistroMovimientoDTO dto) {
+        RegistroMovimientoResponseDTO response=movimientoService.registrarMovimiento(dto);
+         URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path("movimientos/{id}")
+                .buildAndExpand(response.id())
+                .toUri();
+
+        return ResponseEntity.created(location).body(response);
+    }
+
+    @GetMapping("/get-all")
+    public ResponseEntity<?> getAllMovimientos() {
+        return ResponseEntity.ok().body(movimientoService.listAllMovimientos());
+    }
+    
+    
+    
+
+}
