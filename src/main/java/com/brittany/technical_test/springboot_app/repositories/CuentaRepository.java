557e1b2a762e94ba86360b98fe1014a9dc054e65
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-07-24 20:28:11 -0500

    refactor: usar String en lugar de UUID como id por compatibilidad con peticiones en el repositorio de la entidad Cuenta.

--- src/main/java/com/brittany/technical_test/springboot_app/repositories/CuentaRepository.java
+++ src/main/java/com/brittany/technical_test/springboot_app/repositories/CuentaRepository.java
@@ -1,13 +1,11 @@
 package com.brittany.technical_test.springboot_app.repositories;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.brittany.technical_test.springboot_app.models.Cuenta;
 import com.brittany.technical_test.springboot_app.models.TipoCuentaEnum;
 
-public interface CuentaRepository extends JpaRepository<Cuenta, UUID> {
+public interface CuentaRepository extends JpaRepository<Cuenta, String> {
 
     boolean existsByClienteIdAndTipoCuenta(Long clienteId, TipoCuentaEnum tipoCuentaEnum);
 
